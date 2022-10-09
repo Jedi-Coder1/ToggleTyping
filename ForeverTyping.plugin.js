6eb02ac84d1e7c44decf393ec7029944d8d2eaf6
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2022-10-09 09:14:41 -0600

    First Commit Of File

---
+++ ForeverTyping.plugin.js
@@ -0,0 +1,8 @@
+/**
+ * @name ForeverTyping
+ * @author JediMinecraft1
+ * @version 1
+ * @description This Plugin Is To Toggle The Typing Indicator Under The Chat Bar
+ * @website https://github.com/Jedi-Coder1/ForeverTyping
+ * @source 
+ */
